e2796fef0a953f6c64e0a30a75da4cbbb4480841
JakubMizera <kubamiz53@gmail.com>
2023-01-11 15:24:34 +0100

    v1

---
+++ my-app/src/components/App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react';
+import ProductsList from './ProductsList';
+import './../style/reset.css';
+
+const App: FC = () => {
+  return (
+    <>
+      <ProductsList />
+    </>
+  );
+};
+
+export default App;
